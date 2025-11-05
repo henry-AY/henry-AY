6cf3ffa0b00de0df60955ea337767b85aa24c351
henry-AY <henryyost402@gmail.com>
2025-11-04 20:42:07 -0800

    updated profile

---
+++ text.cpp
@@ -0,0 +1,3 @@
+Im a Data Science student at the University of California, Riverside (UCR), passionate about building intelligent systems that bridge data and real-world impact.
+• Python, R, C++, Java, HTML/CSS, React, Node.js, Godot
+• My interests include: AI/ML, data visualization, backend systems, and CI/CD pipelines.
